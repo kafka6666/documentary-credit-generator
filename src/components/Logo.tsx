453defcd3b7e126c48e9cdf5e7ed30242028a35b
kafka6666 <140368515+kafka6666@users.noreply.github.com>
2025-03-09 16:08:20 +0600

    feat: logo and web app name added

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <Link href="/">
+    <Link href="/" className="flex items-center gap-2">
+      <Image src="/logo.png" width={32} height={32} alt="Logo"/>
       <h1 className="text-xl font-bold hover:text-blue-500 transition-colors">
         Autogen AI
       </h1>
